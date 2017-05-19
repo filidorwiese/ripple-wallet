92478cd8248120d096d1a81319329dca9028c297
Filidor Wiese <fili@fili.nl>
2017-05-19 16:18:10 +0200

    Adds Inquirer as a cli user prompt

--- scripts/balance.js
+++ scripts/balance.js
@@ -1,12 +1,30 @@
 'use strict'
+const inquirer = require('inquirer')
 const RippleAPI = require('ripple-lib').RippleAPI
 
-const api = new RippleAPI({server: 'wss://s1.ripple.com:443'})
-const address = ''
+const questions = [
+  {
+    type: 'input',
+    name: 'wallet',
+    message: 'Enter wallet address:',
+    validate: (value) => !!value
+  }
+]
 
-api.connect().then(() => {
-  api.getBalances(address).then(balances => {
-    console.log(JSON.stringify(balances, null, 2))
-    process.exit(0)
+inquirer.prompt(questions).then((answers) => {
+
+  const api = new RippleAPI({server: 'wss://s1.ripple.com:443'})
+
+  api.connect().then(() => {
+    try {
+      api.getBalances(answers.wallet).then(balances => {
+        console.log(JSON.stringify(balances, null, 2))
+        process.exit(0)
+      })
+    } catch (e) {
+      console.error('Invalid address')
+      process.exit(1)
+    }
   })
+
 })
--- scripts/make-payment.js
+++ scripts/make-payment.js
@@ -1,63 +1,105 @@
 'use strict'
+const inquirer = require('inquirer')
 const RippleAPI = require('ripple-lib').RippleAPI
 
-const sourceAddress = ''
-const sourceSecret = ''
-const destinationAddress = ''
-const destinationTag = 0
-const xrpAmount = '5'
-
-const api = new RippleAPI({
-  server: 'wss://s1.ripple.com:443'
-})
-
-const instructions = {
-  maxLedgerVersionOffset: 5,
-  maxFee: '0.15'
-}
-
-const payment = {
-  source: {
-    address: sourceAddress,
-    maxAmount: {
-      value: xrpAmount,
-      currency: 'XRP'
-    }
+const questions = [
+  {
+    type: 'input',
+    name: 'amount',
+    message: 'Enter XRP amount to send:',
+    validate: (value) => isNaN(parseInt(value)) ? 'Please enter a number' : true
   },
-  destination: {
-    address: destinationAddress,
-    tag: destinationTag,
-    amount: {
-      value: xrpAmount,
-      currency: 'XRP'
-    }
+  {
+    type: 'input',
+    name: 'destinationAddress',
+    message: 'Enter destination address:',
+    validate: (value) => !!value
+  },
+  {
+    type: 'input',
+    name: 'destinationTag',
+    message: 'Enter destination tag (optional):',
+    validate: (value) => value && isNaN(parseInt(value)) ? 'Please enter a number' : true,
+    filter: (value) => value && parseInt(value) || ''
+  },
+  {
+    type: 'input',
+    name: 'sourceAddress',
+    message: 'Enter sender address:',
+    validate: (value) => !!value
+  },
+  {
+    type: 'input',
+    name: 'sourceSecret',
+    message: 'Enter sender secret:',
+    validate: (value) => !!value
+  },
+  {
+    type: 'confirm',
+    name: 'sure',
+    message: 'Ready to send?'
   }
-}
+]
 
-function quit (message) {
-  console.log(message)
-  process.exit(0)
-}
+inquirer.prompt(questions).then((answers) => {
+  if (!answers.sure) {
+    process.exit()
+  }
 
-function fail (message) {
-  console.error(message)
-  process.exit(1)
-}
-
-api.connect().then(() => {
-
-  console.log('Connected...')
-
-  return api.preparePayment(sourceAddress, payment, instructions).then(prepared => {
-
-    console.log('Payment transaction prepared...')
-
-    const {signedTransaction} = api.sign(prepared.txJSON, sourceSecret)
-
-    console.log('Payment transaction signed...')
-
-    api.submit(signedTransaction).then(quit, fail)
-
+  const api = new RippleAPI({
+    server: 'wss://s1.ripple.com:443'
   })
 
-}).catch(fail)
+  const instructions = {
+    maxLedgerVersionOffset: 5,
+    maxFee: '0.15'
+  }
+
+  const payment = {
+    source: {
+      address: answers.sourceAddress,
+      maxAmount: {
+        value: answers.amount,
+        currency: 'XRP'
+      }
+    },
+    destination: {
+      address: answers.destinationAddress,
+      tag: answers.destinationTag || undefined,
+      amount: {
+        value: answers.amount,
+        currency: 'XRP'
+      }
+    }
+  }
+
+  function quit (message) {
+    console.log(message)
+    process.exit(0)
+  }
+
+  function fail (message) {
+    console.error(message)
+    process.exit(1)
+  }
+
+  api.connect().then(() => {
+
+    console.log('Connected...')
+
+    return api.preparePayment(answers.sourceAddress, payment, instructions).then(prepared => {
+
+      console.log('Payment transaction prepared...')
+
+      const {signedTransaction} = api.sign(prepared.txJSON, answers.sourceSecret)
+
+      console.log('Payment transaction signed...')
+
+      api.submit(signedTransaction).then(quit, fail)
+
+    })
+
+  }).catch(fail)
+
+})
+
